cb7496b0e06b992b617843526aa263fb424f3a9b
Brandon Reiley <breiley@sdsu.edu>
2020-11-16 18:26:53 -0800

    added some script and animations

---
+++ Assets/Code/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float jumpPower;
+    private float moveInput;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpPower;
+        }
+    }
+}
